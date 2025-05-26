5bbcadd57bf6e85a48c383a5bea0611a2fb83a14
i-amnt <judith.schmiedchen@gmail.com>
2025-05-26 18:39:44 +0200

    Update header

--- model.h
+++ model.h
@@ -1,13 +1,27 @@
 #ifndef MODEL_H
 #define MODEL_H
 #include <string>
+#include <vector>
 
 using namespace std;
+
+// Vertex Koordinaten
+struct Vertex {
+	float x, y, z;
+};
+
+struct Facet {
+	Vertex normal; // Flächennormale
+	Vertex vertices[3]; // Eckpunkte Dreieck
+};
 
 class Model {
 	public:
 		Model(); //Constructor (empty atm)
 		void loadModel(string file_name);
+	private:
+		// speichert Liste von Facets in einem Vektor
+		vector<Facet> modelData;
 };
 
 #endif
