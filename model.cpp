68b0352cad4f94eecca2c954a287393a6b3ef971
i-amnt <judith.schmiedchen@gmail.com>
2025-05-26 18:37:23 +0200

    Feat: Add Da

--- model.cpp
+++ model.cpp
@@ -11,15 +11,36 @@ Model::Model(){}; //Constructor (empty atm)
 //Load Model file
 void Model::loadModel(string file_name) {
 	ifstream file(file_name);
+	vector<Facet> modelData; 
 			
 	if (file.is_open()) {
 		string line;
+		Facet facet;
+		int vertexCount = 0; // damit wird gezählt, wie viele "vertex"-Zeilen es waren, weil Dreiecke brauchen genau 3 Seiten (=Zeile)		
 		
 		while (getline(file, line)) {
-			cout << line.c_str() << endl;
+		istringstream iss(line);
+        string word;
+		iss >> word;
+
+			// wenn Zeile mit "facet" losgeht: parset Normale. für "vertex": parset Vertexkoordinaten
+			if (word == "facet") {
+				iss >> word; 
+				iss >> facet.normal.x >> facet.normal.y >> facet.normal.z; // Werte extrahieren
+			} else if (word == "vertex") {
+				iss >> facet.vertices[vertexCount].x >> facet.vertices[vertexCount].y >> facet.vertices[vertexCount].z;
+				vertexCount++;
+			}
+
+			// nach drei eingelesen "vertex"-Zeilen/ Seiten wird Facet abgespeichert
+			if (vertexCount == 3) {
+				modelData.push_back(facet);
+				vertexCount = 0; // Reset 
+			}
 		}
-			
-	file.close();
-	system("pause");
+
+		file.close();
+
+		cout << "Geladene Facetten: " << modelData.size() << endl;
 	}
 }	
