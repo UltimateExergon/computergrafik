706641f5bb7c84f6e5e0baa27ef7c9e9ffc4de62
i-amnt <judith.schmiedchen@gmail.com>
2025-06-04 23:05:03 +0200

    add function get_pixel

--- camera.cpp
+++ camera.cpp
@@ -1,7 +1,13 @@
+#include <cmath>
 #include "camera.h"
 
+Camera::Camera() {
+    screenWidth = 2.0f;
+    screenHeight = 2.0f;
+    imageWidth = 1000; 
+    imageHeight = 1000;
+}
 
-Camera::Camera(){};
 
 int Camera::get_imageWidth(){
 	return imageWidth;
@@ -10,3 +16,28 @@ int Camera::get_imageWidth(){
 int Camera::get_imageHeight(){
 	return imageHeight;
 }
+
+
+Pos3D Camera::get_pixel(int x, int y) const {
+    // Berechne den Mittelpunkt des Bildschirms in Weltkoordinaten
+    Pos3D screenCenter = { cameraPos.x + cameraView.x, 
+                           cameraPos.y + cameraView.y, 
+                           cameraPos.z + cameraView.z };
+
+    // Berechne die Größe eines Pixels in Weltkoordinaten
+    float pixelWidth = screenWidth / imageWidth;
+    float pixelHeight = screenHeight / imageHeight;
+
+    // Berechne die Offset-Werte zur Position des Pixels relativ zur Mitte
+    float offsetX = (x - imageWidth / 2.0f) * pixelWidth;
+    float offsetY = (y - imageHeight / 2.0f) * pixelHeight;
+
+    // Berechne die endgültige Pixelposition
+    Pos3D pixelPos = { screenCenter.x + offsetX, 
+                       screenCenter.y + offsetY, 
+                       screenCenter.z };
+
+    return pixelPos;
+}
+
+
